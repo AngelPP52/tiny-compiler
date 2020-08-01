ca7ded4cd06ba5b5b5bcdd873922a14774c218f1
LghAndHpg <1017118152@qq.com>
2020-08-02 02:30:27 +0800

    语法分析器

--- src/parser.js
+++ src/parser.js
@@ -1,1 +1,106 @@
 // 语法分析，生成ast
+const { tokenizer } = require('./tokenizer');
+
+function parser(code) {
+    let tokens = tokenizer(code);
+    console.log(tokens);
+    let current = 0; // tokens移动指针
+    function walk(parent) {
+        let token = tokens[current]; // 当前单词
+        let next = tokens[current + 1]; // 下一个单词，因为这里需要预判一下当前<是属于开分号还是闭分号
+        if (token.type === 'parenLeft' && token.value == '<' && next.type === 'label') {
+            let node = {
+                type: 'JSXElement',
+                openingElement: null,
+                closingElement: null,
+                children: []
+            }
+            token = tokens[++current]; // 标签
+            let labelName = token.value;
+            node.openingElement = {
+                type: 'JSXOpeningElement',
+                name: {
+                    type: "JSXIdentifier",
+                    name: token.value
+                },
+                attributes: []
+            }
+            token = tokens[++current]; // 跳过标签
+
+            while (token.type != 'parenRight' && token.type === "attributeKey") { // walk所有属性
+                node.openingElement.attributes.push(walk());
+                token = tokens[current];
+            }
+            token = tokens[++current]; // 跳过>单词
+            next = tokens[current+1]; // 下一个单词，反斜杠？
+            next_next = tokens[current+2]; // 下一个单词，标签名？
+
+            while (token.type !== 'parenLeft' || token.type === 'parenLeft' && next.type !== 'backSlash' && next_next.value !== labelName) { // walk所有孩子
+                node.children.push(walk());
+                token = tokens[current];
+                next = tokens[current+1]; // 下一个单词，反斜杠？
+                next_next = tokens[current+2]; // 下一个单词，标签名？
+            }
+
+            node.closingElement = walk(node); // walk闭合标签
+            ++current;
+            return node;
+        } 
+        else if(parent && token.type === 'parenLeft' && token.value == '<' && next.type === 'backSlash'){
+            current++;
+            token = tokens[++current]; // 闭合标签
+            current++;
+            return parent.closingElement = {
+                type: "JSXClosingElement",
+                name: {
+                    type: "JSXIdentifier",
+                    name: token.value
+                }
+            }
+        }
+        else if(token.type === "attributeKey"){
+            let next = tokens[++current]; // attributeValue // 只处理了attributeStringValue的情况
+            let node = {
+                type: "JSXAttribute",
+                name: {
+                    type: "JSXIdentifier",
+                    name: token.value
+                },
+                value: {
+                    type: "StringLiteral",
+                    value: next.value
+                }
+            }
+            current++; // 跳过attributeValue
+            return node
+        }
+        else if(token.type === "text"){
+            current++; // 跳过>单词
+            return {
+                text: "JSXText",
+                value: token.value
+            }
+        }
+        throw new TypeError(token.type);
+    }
+
+    // ast根节点
+    var ast = {
+        type: 'Program',
+        body: [
+            {
+                type: 'ExpressionStatement',
+                expression: null
+            }
+        ]
+    };
+
+    // walk
+    ast.body[0].expression = walk();
+
+    return ast;
+}
+
+let code = '<h1 id="title"><span id="name">hello</span>world</h1>';
+
+console.log(JSON.stringify(parser(code), null, 2));
--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,155 +1,162 @@
 // 词法分析，分词
 // 有限状态机
 let LETTERS = /^[a-zA-Z0-9_]$/;
-let NOT_LEFT_SEM = /^</
+let NOT_LEFT_SEM = /^</;
 
-let currentToken={type:"",value:""};
+let currentToken = { type: "", value: "" };
 let tokens = [];
 
-
-
-function emit(token){
-    console.log(token);
+function emit(token) {
+    // console.log(token);
+    currentToken = { type: "", value: "" };
     tokens.push(token);
 }
 
-function start(char){
-    if(char === "<"){
-        emit({type: "parenLeft",value:"<"})
+function start(char) {
+    if (char === "<") {
+        emit({ type: "parenLeft", value: "<" });
         return foundParenL;
     }
     return start;
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
-function foundParenL(char){
-    if(LETTERS.test(char)){
-        currentToken.value = char
+function foundParenL(char) {
+    if (LETTERS.test(char)) {
+        currentToken.value = char;
         currentToken.type = "label";
         return label;
-    }else if(char === "/"){
-        emit({type: "backSlash",value:"/"})
+    } else if (char === "/") {
+        emit({ type: "backSlash", value: "/" });
         return foundParenL;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function foundParenR(char) {
-    if(char === "<"){
-        emit({type: "parenLeft",value:"<"})
+    if (char === "<") {
+        emit({ type: "parenLeft", value: "<" });
         return foundParenL;
-    }else{ // 文本节点
+    } else {
+        // 文本节点
         currentToken.value = char;
         currentToken.type = "text";
         return text;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
-function label(char){
-    if(LETTERS.test(char)){
+function label(char) {
+    if (LETTERS.test(char)) {
         currentToken.value += char;
         return label;
-    }else if(char === " "){ // 空格不输出
+    } else if (char === " ") {
+        // 空格不输出
         emit(currentToken);
         return attribute;
-    }else if(char === ">"){
+    } else if (char === ">") {
         emit(currentToken);
-        emit({type: "parenRight",value:">"})
+        emit({ type: "parenRight", value: ">" });
         return foundParenR;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function attribute(char) {
-    if(LETTERS.test(char)){
+    if (LETTERS.test(char)) {
         currentToken.value = char;
         currentToken.type = "attributeKey";
         return attributeKey;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function attributeKey(char) {
-    if(LETTERS.test(char)){
+    if (LETTERS.test(char)) {
         currentToken.value += char;
         return attributeKey;
-    }else if(char === "="){
+    } else if (char === "=") {
         emit(currentToken);
-        emit({type: "equalSign", value: "="})
+        // emit({type: "equalSign", value: "="})
         return attributeValue;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function attributeValue(char) {
-    if(char === "\""){
-        emit({type: "quoMarkLeft", value: "\""});
+    if (char === '"') {
+        // emit({type: "quoMarkLeft", value: "\""});
         currentToken.value = "";
         currentToken.type = "attributeStringValue";
         return attributeStringValue;
-    }else if(char === "{"){
-        emit({type: "bracketL", value: "{"});
+    } else if (char === "{") {
+        // emit({type: "bracketL", value: "{"});
         currentToken.value = "";
         currentToken.type = "attributeJSXValue";
         return attributeJSXValue;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function attributeStringValue(char) {
     if (LETTERS.test(char)) {
         currentToken.value += char;
         return attributeStringValue;
-    }else if(char === "\""){
+    } else if (char === '"') {
         emit(currentToken);
-        emit({type: "quoMarkRight", value:"\""})
+        // emit({type: "quoMarkRight", value:"\""})
         return tryLeaveAttribute;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function attributeJSXValue(char) {
     if (LETTERS.test(char)) {
         currentToken.value += char;
         return attributeJSXValue;
-    }else if(char === "}"){
+    } else if (char === "}") {
         emit(currentToken);
-        emit({type: "bracketR", value:"}"})
+        // emit({type: "bracketR", value:"}"})
         return tryLeaveAttribute;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function tryLeaveAttribute(char) {
-    if(char === " "){
+    if (char === " ") {
         return attribute;
-    }else if(char === ">"){
-        emit({type:"parenRight",value:">"});
+    } else if (char === ">") {
+        emit({ type: "parenRight", value: ">" });
         return foundParenR;
     }
-    throw new TypeError('Error')
+    throw new TypeError("Error");
 }
 
 function text(char) {
-    if(char === "<"){
+    if (char === "<") {
         emit(currentToken);
-        emit({type: "parenLeft",value:"<"})
+        emit({ type: "parenLeft", value: "<" });
         return foundParenL;
-    }else{ // 文本内容
+    } else {
+        // 文本内容
         currentToken.value += char;
         return text;
     }
 }
 
+let code = '<h1 id="title"><span>hello</span>world</h1>';
 
-let string = '<h1 id="title"><span>hello</span>world</h1>';
+tokenizer(code);
 
-tokenizer(string);
-
-function tokenizer(input){
+function tokenizer(input) {
+    tokens = [];
     let state = start;
-    for(let char of input){
+    for (let char of input) {
         state = state(char);
     }
+    return tokens;
 }
+
+module.exports = {
+    tokenizer,
+};
